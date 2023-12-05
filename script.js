0360951958462a2f8988478002a559d384268d9d
PauloFonseca <pfonseca.arq@gmail.com>
2023-12-05 15:56:21 -0300

    20. Crie um contador de caracteres.

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const inputEmail = document.getElementById('email');
 const inputPassword = document.getElementById('password');
 const btnSubmit = document.getElementById('submit-btn');
 const checkBox = document.getElementById('agreement');
+const textArea = document.getElementById('textarea');
+const characters = document.getElementById('counter');
 
 btnLogin.addEventListener('click', () => {
   const email = 'tryber@teste.com';
@@ -20,3 +22,15 @@ const checked = () => {
 };
 
 checkBox.addEventListener('change', checked);
+
+const counterCharacter = () => {
+  const character = textArea.value.length;
+
+  const maxCharacter = 500;
+
+  const counter = maxCharacter - character;
+
+  characters.innerText = `${counter} caracteres`;
+};
+
+textArea.oninput = counterCharacter;
