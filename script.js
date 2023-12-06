fee58dbcabe997abd50c809e25750c37ddd08293
PauloFonseca <pfonseca.arq@gmail.com>
2023-12-06 13:57:25 -0300

    21 - Substitua o formulário pelas informações da pessoa estudante

--- script.js
+++ script.js
@@ -5,12 +5,27 @@ const btnSubmit = document.getElementById('submit-btn');
 const checkBox = document.getElementById('agreement');
 const textArea = document.getElementById('textarea');
 const characters = document.getElementById('counter');
+const forms = document.getElementById('evaluation-form');
+const main = document.getElementById('main');
+const firstName = document.getElementById('input-name');
+const lastName = document.getElementById('input-lastname');
+const email = document.getElementById('input-email');
+const house = document.getElementById('house');
+const family = document.getElementsByName('family');
+const hofs = document.getElementById('hofs');
+const jest = document.getElementById('jest');
+const promises = document.getElementById('promises');
+const react = document.getElementById('react');
+const sql = document.getElementById('sql');
+const python = document.getElementById('python');
+const rate = document.getElementsByName('rate');
+const observation = document.getElementById('textarea');
 
 btnLogin.addEventListener('click', () => {
-  const email = 'tryber@teste.com';
+  const testEmail = 'tryber@teste.com';
   const passoword = '123456';
 
-  if (email === inputEmail.value && passoword === inputPassword.value) {
+  if (testEmail === inputEmail.value && passoword === inputPassword.value) {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
@@ -34,3 +49,113 @@ const counterCharacter = () => {
 };
 
 textArea.oninput = counterCharacter;
+
+const createName = (creatDiv) => {
+  const name = document.createElement('p');
+  creatDiv.appendChild(name);
+  name.textContent = `Nome: ${firstName.value} ${lastName.value}`;
+};
+
+const createEmail = (creatDiv) => {
+  const name = document.createElement('p');
+  creatDiv.appendChild(name);
+  name.textContent = `Email: ${email.value}`;
+};
+
+const createHouse = (creatDiv) => {
+  const name = document.createElement('p');
+  creatDiv.appendChild(name);
+  name.textContent = `Casa: ${house.value}`;
+};
+
+const createFamily = (creatDiv) => {
+  let familySelectd = [];
+
+  for (let i = 0; i < family.length; i += 1) {
+    if (family[i].checked) {
+      familySelectd = family[i];
+    }
+  }
+
+  const name = document.createElement('p');
+  creatDiv.appendChild(name);
+
+  if (familySelectd.value === undefined) {
+    name.textContent = 'Família:';
+  } else {
+    name.textContent = `Família: ${familySelectd.value}`;
+  }
+};
+
+const responseContent = () => {
+  const content = [hofs, jest, promises, react, sql, python];
+  const contentSelected = [];
+  for (let i = 0; i < content.length; i += 1) {
+    if (content[i].checked) {
+      contentSelected.push(content[i].value);
+    }
+  }
+  return contentSelected;
+};
+
+const createContent = (creatDiv) => {
+  const contentSelected = responseContent();
+  let contentSelectedString = '';
+  const name = document.createElement('p');
+  creatDiv.appendChild(name);
+
+  for (let i = 0; i < contentSelected.length; i += 1) {
+    contentSelectedString += `${contentSelected[i]}, `;
+  }
+  const newString = contentSelectedString.slice(0, -2);
+  name.textContent = `Matérias: ${newString}`;
+
+  if (contentSelected) {
+    name.textContent = `Matérias: ${newString}`;
+  } else {
+    name.textContent = 'Matérias:';
+  }
+};
+
+const createRate = (creatDiv) => {
+  let rateSelectd = [];
+
+  for (let i = 0; i < rate.length; i += 1) {
+    if (rate[i].checked) {
+      rateSelectd = rate[i];
+    }
+  }
+
+  const name = document.createElement('p');
+  creatDiv.appendChild(name);
+
+  if (rateSelectd.value === undefined) {
+    name.textContent = 'Avaliação:';
+  } else {
+    name.textContent = `Avaliação: ${rateSelectd.value}`;
+  }
+};
+
+const createObservation = (creatDiv) => {
+  const name = document.createElement('p');
+  creatDiv.appendChild(name);
+  name.textContent = `Observações: ${observation.value}`;
+};
+
+btnSubmit.addEventListener('click', (e) => {
+  e.preventDefault();
+  forms.style.display = 'none';
+  const creatDiv = document.createElement('div');
+  creatDiv.id = 'form-data';
+  main.insertBefore(creatDiv, main.firstChild);
+  const title = document.createElement('h1');
+  creatDiv.appendChild(title);
+  title.textContent = 'Inscrição realizada!';
+  createName(creatDiv);
+  createEmail(creatDiv);
+  createHouse(creatDiv);
+  createFamily(creatDiv);
+  createContent(creatDiv);
+  createRate(creatDiv);
+  createObservation(creatDiv);
+});
